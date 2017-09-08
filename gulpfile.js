28e1ffef92d402e3a72fe957ba96e03688dc1620
Matt Greenberg <magreenberg@fullsail.edu>
2017-09-08 17:21:55 -0600

    Added postcss and auto prefixer to the dev tools

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
 
 gulp.task('default', function(){
 
 });
 
 gulp.task('scss', function(){
+	var processors = [
+	autoprefixer({ browsers: ['last 2 versions']}),
+	];
 	return gulp.src('./scss/*.scss')
 	.pipe(sass())
+	.pipe(postcss(processors))
 	.pipe(gulp.dest('./css'))
 	.pipe(browserSync.reload({
 		stream: true
