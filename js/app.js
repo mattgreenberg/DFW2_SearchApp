771e5e7f0486957ccab51e08cc2477b05604c26d
Matt Greenberg <magreenberg@fullsail.edu>
2017-09-10 06:51:10 -0600

    added local storage saving for the search query

--- js/app.js
+++ js/app.js
@@ -28,6 +28,7 @@ window.addEventListener('load', function(e){
 
 	var searchForm = document.getElementById('searchform');
 	var errorMsg = document.querySelector('p.error');
+	var resultMsg = document.querySelector('div.wrapper h2');
 	var inputField = document.getElementById('search');
 	var content = document.querySelector('div.content');
 	var menu = document.getElementById('hamburger');
@@ -50,8 +51,12 @@ window.addEventListener('load', function(e){
 		errorMsg.style.display = "none";
 	});
 
-	// load initial
-	request("https://api.themoviedb.org/3/search/movie?api_key=9e27466f28a16d90e4906782aa96b80c&language=en-US&query=the+kids&page=1&include_adult=false");
+	if(localStorage.getItem('term')){
+		var term = localStorage.getItem('term');
+		request("https://api.themoviedb.org/3/search/movie?api_key=9e27466f28a16d90e4906782aa96b80c&language=en-US&query="+term+"&page=1&include_adult=false", term);
+	} else {
+		request("https://api.themoviedb.org/3/search/movie?api_key=9e27466f28a16d90e4906782aa96b80c&language=en-US&query=the+kids&page=1&include_adult=false", "the kids");
+	}
 
 	searchForm.addEventListener('submit', function(e){
 
@@ -64,11 +69,11 @@ window.addEventListener('load', function(e){
 		url += "query=" + searchTerm + "&"
 		url += "page=1&";
 		url += "include_adult=false";
-		request(url);
+		request(url, searchTerm);
 
 	});
 
-	function request(url){
+	function request(url, term){
 
 		window.ajax(url, function(results){
 			if(!results){
@@ -77,22 +82,24 @@ window.addEventListener('load', function(e){
 				inputField.select();	
 			} else {
 				var data = JSON.parse(results);
-				loadAssets(data);
+				loadAssets(data, term);
 			};
 		});
 
 	};
 
 
-	function loadAssets(data){
+	function loadAssets(data, term){
 
 		console.dir(data);
 		if(data.total_results === 0){
 			errorMsg.innerHTML = "Error : No Results Found";
 			errorMsg.style.display = "block";
-			inputField.select();
 		} else {
-
+			// since the search was good, save it in web storage
+			localStorage.setItem('term', term);
+			term = term.replace("+", " ");
+			resultMsg.innerHTML = "Showing Results For '"+term+"':";
 			var len = (data.results.length > 6)? 6 : data.results.length;
 			content.innerHTML = "";
 			for(var i=0; i<len; i++){
@@ -127,7 +134,7 @@ window.addEventListener('load', function(e){
 			}
 
 		}
-
+		inputField.select().focus();
 	};
 
 
